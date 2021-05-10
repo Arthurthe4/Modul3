d8e4c640a16f2fa3326469f33730612378abc184
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-05-10 13:43:01 +0200

    a

---
+++ ConsoleApp01/ConsoleApp01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250; 
+            var counts = new int[range]; 
+            string text = "something"; 
+
+            while (!string.IsNullOrWhiteSpace(text)) 
+            {
+                text = Console.ReadLine(); 
+                UpdateCharCounts(text, counts);
+                ShowCounts(range, counts);
+            }
+        }
+
+        private static void ShowCounts(int, range, int[] counts) 
+        {
+            for (var i = 0; i < range; i++)
+            {
+                if (counts[i] == 0) continue;
+                var character = (char)i;
+                Console.WriteLine("\"" + character + "\" - " + counts[i]);
+            }
+        }
+
+        private static void UpdateCharCounts(string text, int[] counts) 
+        {
+            foreach (var character in text) 
+            {
+                counts[(int)character]++;
+            }
+        }
+    }
+}
